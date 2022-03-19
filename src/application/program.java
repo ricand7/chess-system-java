163131702b7999d6a02d3a0dd9277713316bd765
ricand7 <ricantonioj@hotmail.com>
2022-03-19 08:14:33 -0300

    feat Starting to implement board and piece

--- src/application/program.java
+++ src/application/program.java
@@ -6,8 +6,7 @@ public class program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-      Position pos = new Position(3, 5);
-      System.out.println(pos);
+      Board board = new Board(8 ,8);
 	  
 	}
 
